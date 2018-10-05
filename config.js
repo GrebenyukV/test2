1e846a1faaeef891d71195124d593e3185b6c4ec
GrebenyukV <35668128+GrebenyukV@users.noreply.github.com>
2018-10-05 19:03:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // telegram token
-var token = '698720893:AAEMZra5GApM1zAbHD97pPpjgSdOgJ9PiG4'
+var token = '651232728:AAFHWVODBx386mQrxb-tskl_j67Pk_YK6yU'
 // firebase config
 var config = {
-    apiKey: "AIzaSyBAu6N0lMSbUA7YNv70l3N7h-iVMGFHRL8",
-    authDomain: "airdrop-bot-b8427.firebaseapp.com",
-    databaseURL: "https://airdrop-bot-b8427.firebaseio.com",
-    projectId: "airdrop-bot-b8427",
-    storageBucket: "airdrop-bot-b8427.appspot.com",
-    messagingSenderId: "13313106206"
+    apiKey: "AIzaSyDH6L91HrzJp3BFVrlNG-8_YpZdSCTLRdA",
+    authDomain: "telegrambot-1fe75.firebaseapp.com",
+    databaseURL: "https://telegrambot-1fe75.firebaseio.com",
+    projectId: "telegrambot-1fe75",
+    storageBucket: "telegrambot-1fe75.appspot.com",
+    messagingSenderId: "83059392498"
 };
 
 exports.token = token;
